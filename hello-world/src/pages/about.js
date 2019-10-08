1852059e8661f33770af51e9432e4a6b1938557a
Bao Pham <ryanvuepham@gmail.com>
2019-10-08 01:02:25 -0500

    Gatsby building blocks

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+export default () => (
+  <div style={{ color: `dodgerblue` }}>
+    <Header headerText="About Gatsby, baby" />
+    <Header headerText="It's pretty damn cool" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: "purple" }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Batsby!" />
+    <p>Demon Slayer</p>
+    <img
+      src="https://4.bp.blogspot.com/-vIaYEfwZBAw/XVHIWe1RzmI/AAAAAAAALEA/85QRWriKStwnYQL1IK29noPQCOUrCjsCQCLcBGAs/w914-h514-p-k-no-nu/tanjiro-kamado-flaming-katana-kimetsu-no-yaiba-uhdpaper.com-4K-3.427-wp.thumbnail.jpg"
+      alt=""
+    />
+  </div>
+)
